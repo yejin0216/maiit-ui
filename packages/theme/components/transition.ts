17fdc9b5d7f33ab3c525fd2d95a1bb1686c67da7
yejin <yejin0216@gmail.com>
2021-08-28 10:14:35 +0900

    트랜지션 변수 추가

---
+++ packages/theme/components/transition.ts
@@ -0,0 +1,15 @@
+export const TransitionTimeouts = {
+  Fade: 150,
+  Collapse: 350,
+  Modal: 300,
+  Carousel: 600,
+};
+
+export const TransitionPropTypeKeys = [
+  'onEnter',
+  'onEntering',
+  'onEntered',
+  'onExit',
+  'onExiting',
+  'onExited',
+];
